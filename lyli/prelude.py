74f8a27d0a1398060a1100119bae3e2d5da3cf55
Loïc Dauphin <astralien3000@yahoo.fr>
2022-06-30 21:52:48 +0200

    continue fn stmt

--- lyli/prelude.py
+++ lyli/prelude.py
@@ -3,6 +3,8 @@ import lyli.func as func
 import lyli.eval as eval
 import lyli.ast as ast
 
+import operator as op
+
 
 S = ast.Symbol
 C = ast.Call
@@ -16,30 +18,50 @@ def _file(ctx, *args):
 
 
 class Stmt:
-  matchers = []
 
-  @staticmethod
-  def matcher(function):
-    Stmt.matchers.append(function)
+  class Next:
+
+    def __init__(self, matchers, ctx, *args):
+      self.matchers = matchers
+      self.ctx = ctx
+      self.args = args
+
+    def __call__(self):
+      if len(self.matchers) == 0:
+        raise Exception(
+          f"Could not match {self.args}"
+        )
+      else:
+        return self.matchers[0](
+          Stmt.Next(
+            self.matchers[1:],
+            self.ctx,
+            *self.args,
+          ),
+          self.ctx,
+          *self.args,
+        )
+
+  def __init__(self, matchers = []):
+    self.matchers = matchers
+
+  def matcher(self, function):
+    self.matchers.append(function)
     return function
-  
-  @staticmethod
-  def final(matchers, ctx, *args):
-    raise Exception(
-      f"Could not match {args}"
-    )
 
-  @staticmethod
-  def stmt(ctx, *args):
-    return Stmt.matchers[0](
-      [*Stmt.matchers[1:], Stmt.final],
+  def __call__(self, ctx, *args):
+    return Stmt.Next(
+      self.matchers,
       ctx,
-      *args
-    )
+      *args,
+    )()
 
 
-@Stmt.matcher
-def _stmt_let(matchers, ctx, *args):
+_stmt = Stmt()
+
+
+@_stmt.matcher
+def _stmt_let(next, ctx, *args):
   match args:
     case [S("let"), S(var_name), S("="), val_expr]:
       ctx, value = eval.eval(ctx, val_expr)
@@ -47,32 +69,47 @@ def _stmt_let(matchers, ctx, *args):
         var_name: value,
       })
       return new_ctx, None
-  return matchers[0](matchers[1:], ctx, *args)
+  return next()
 
 
-@Stmt.matcher
-def _stmt_print(matchers, ctx, *args):
+@_stmt.matcher
+def _stmt_print(next, ctx, *args):
   match args:
     case [S("print"), *val_exprs]:
       print(*val_exprs)
       return ctx, None
-  return matchers[0](matchers[1:], ctx, *args)
+  return next()
 
 
-@Stmt.matcher
-def _stmt_fn(matchers, ctx, *args):
+@_stmt.matcher
+def _stmt_fn(next, ctx, *args):
   match args:
-    case [S("fn"), C([S(fn_name), *fn_args]), S("->"), C([fn_ret_type, *fn_body]), *others]:
+    case [S("fn"), C([S(fn_name), *fn_args]), S("->"), C([fn_ret_type, *fn_body])]:
       new_ctx = context.Context(ctx)
-      f = func.LyliFunc([], fn_body, new_ctx)
+      f = func.LyliFunc(fn_args, fn_body, new_ctx)
       new_ctx[fn_name] = f
       return new_ctx, f
-  return matchers[0](matchers[1:], ctx, *args)
+    case [S("fn"), C([C([S(fn_name), *fn_args]), *fn_body])]:
+      new_ctx = context.Context(ctx)
+      f = func.LyliFunc(fn_args, fn_body, new_ctx)
+      new_ctx[fn_name] = f
+      return new_ctx, f
+  return next()
+
+
+@_stmt.matcher
+def _stmt_add(next, ctx, *args):
+  match args:
+    case [left_expr, S("+"), right_expr]:
+      return eval.eval(ctx, C([S("+"), left_expr, right_expr]))
+  return next()
 
 
 prelude_ctx = context.Context({
   "file": func.PyMacro(_file),
-  "stmt": func.PyMacro(Stmt.stmt),
+  "stmt": func.PyMacro(_stmt),
 
   "print": func.PyFunc(print),
+
+  "+": func.PyFunc(op.add),
 })
